7274450a82f12d34014ed09a75ca5e34072334bf
cesarpachon <cesarpachon@gmail.com>
2019-01-14 09:58:06 -0500

    adding unitusti experiment

--- unitusti/test_editor.js
+++ unitusti/test_editor.js
@@ -8,7 +8,7 @@ window.TestEditor =
     this.options = options;
     let el = document.querySelector(this.options.sel);
     el.innerHTML = 'iframe starts here: <iframe width="300" height="200" src="https://cesarpachon.github.io/unitusti/test_editor.html"></iframe>';
-  }
+  },
 
   unloadEditor : function(cb)
   {
