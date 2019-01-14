9f87271add156d4ebec085d1cb17f1598e565975
cesarpachon <cesarpachon@gmail.com>
2019-01-14 10:22:50 -0500

    adding unitusti experiment

--- unitusti/test_editor.js
+++ unitusti/test_editor.js
@@ -8,6 +8,7 @@ window.TestEditor =
     this.options = options;
     let el = document.querySelector(this.options.sel);
     el.innerHTML = 'iframe starts here: <iframe width="300" height="200" src="https://cesarpachon.github.io/unitusti/test_editor.html"></iframe>';
+    window.addEventListener("message", onMessage);
   },
 
   unloadEditor : function(cb)
@@ -16,9 +17,15 @@ window.TestEditor =
     el.innerHTML = "";
   },
 
+  onMessage : function(ev)
+  {
+    console.log("onMessage");
+    console.log(ev);
+  },
+
   addTarget : function(ev)
   {
-    console.log("addTarget");
+    console.log("addTarget callback");
     let name = "name"+Date.now();
     let description = "description for " + name;
     let metadata = {
